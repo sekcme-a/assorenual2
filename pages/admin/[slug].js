55377ec61b935e6c9d2e53a3be69164b4db6fe7d
eotodcp <eotodcp@gmail.com>
2022-03-06 17:07:43 +0900

    asdf

---
+++ pages/admin/[slug].js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Admin = () => {
+  return (
+    <>
+      asdf
+    </>
+  )
+}
+
+export default Admin;
---
+++ pages/admin/login.js
@@ -0,0 +1,20 @@
+import React from "react"
+import style from "styles/admin/login.module.css"
+
+const Login = () => {
+  return (
+    <div className={style.background}>
+      <div className={style.container}>
+        <div className={style.loginContainer}>
+          <h4>Admin Login</h4>
+          <div className={style.border} />
+        </div>
+        <div className={style.styleContainer}>
+          <h4>국민의 건강과<br/>행복의 장을 여는<br/></h4>
+          <h3>대한생활체육회</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Login;
