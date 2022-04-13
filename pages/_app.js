ebbb2ae57813f2882beda3368adc43b226d511c5
eotodcp <eotodcp@gmail.com>
2022-04-13 15:47:33 +0900

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -39,9 +39,12 @@ function MyApp({ Component, pageProps }) {
     <UserContext.Provider value={userData}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81"></script>
+        {/* <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81"></script> */}
       </Head>
-      {/* <Script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81"></Script> */}
+            <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81&libraries=services,clusterer&autoload=false`}
+        strategy="beforeInteractive"
+      />
       {mobileMode ? <NavbarMobile /> : <Navbar />}
       <Component {...pageProps} />
       <Footer />
--- src/components/info/Map.js
+++ src/components/info/Map.js
@@ -1,45 +1,26 @@
 import React, { useEffect } from 'react'
 import style from "styles/info/location.module.css"
 import Script from "next/script"
+import Link from "next/link"
+import { Map, MapMarker } from "react-kakao-maps-sdk"
 
-const Map =(props)=>{
-
-  useEffect(() => {
-    const x = props.locX;
-    const y = props.locY;
-    let container = document.getElementById('map');
-    let options = {
-      center: new kakao.maps.LatLng(x, y),
-      level: 4
-    };
-
-    let map = new kakao.maps.Map(container, options);
-    let markerPosition  = new kakao.maps.LatLng(x, y); 
-    let marker = new kakao.maps.Marker({
-      position: markerPosition
-    });
-    marker.setMap(map);
-    var iwContent = `<div style="width: 145px; padding:2px 5px; text-align: center; font-weight: bold; font-size: 12px; line-height: 14px">(사)대한생활체육회<br/><a href=https://map.kakao.com/link/map/대한생활체육회,${x},${y} style="color:blue" target="_blank">큰지도보기</a> / <a href="https://map.kakao.com/link/to/대한생활체육회,${x},${y}" style="color:blue" target="_blank">길찾기</a></div>`; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    var  iwPosition = new kakao.maps.LatLng(x, y); //인포윈도우 표시 위치입니다
-
-    // 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-        position : iwPosition, 
-        content : iwContent 
-    });
-
-    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-    infowindow.open(map, marker); 
-
-  }, [])
+const KakaoMap =(props)=>{
       
   return (
-    <>
-      {/* <Script src="https://developers.kakao.com/sdk/js/kakao.js" /> */}
-      <Script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81"></Script>
-      <div id="map" className={style.locationMap}></div>
-    </>
+    <div className={style.map}>
+      <Map
+        center={{ lat: props.locX, lng: props.locY }}
+        style={{ width: "100%", height: "360px" }}
+      >
+        <MapMarker position={{ lat: props.locX, lng: props.locY }}>
+          <div className={style.marker}>(사)대한생활체육회<br />
+            <Link href={`https://map.kakao.com/link/map/대한생활체육회,${props.locX},${props.locY}`}>큰지도보기</Link> /
+            <Link href={`https://map.kakao.com/link/to/대한생활체육회,${props.locX},${props.locY}`}>길찾기</Link>
+          </div>
+        </MapMarker>
+      </Map>
+    </div>
     )
 }
 
-export default Map;
+export default KakaoMap;
