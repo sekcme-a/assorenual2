a24b75e89fe970d18b1dbdb55a4d5f6e52c9fb32
eotodcp <eotodcp@gmail.com>
2022-09-22 18:25:08 +0900

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,21 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserContext.Provider value={userData}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta charSet="UTF-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+          <link rel="canonical" href="https://xn--vk1by6xrzecngs4l6obxj.com"></link>
+          <meta name="robots" content="index,follow"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:image" content="https://xn--vk1by6xrzecngs4l6obxj.com/public/logo-circle.png" />
+          <meta property="og:url" content="https://xn--vk1by6xrzecngs4l6obxj.com"></meta>
+          <meta name="keywords" content="대한생활체육회"/>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+          
+          <meta name="naver-site-verification" content="fcd6939d5a2a544afe2de373e246bedd86d5702d" />
+          <meta name="google-site-verification" content="h0jRizviYwbetdIjDK5dX1iXSEiLWvRJ_JqBAnc7nfg" />
       </Head>
             <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=10143b83ffc3b3f9b4dfefb69908cb81&libraries=services,clusterer&autoload=false`}
--- pages/_document.js
+++ pages/_document.js
@@ -10,22 +10,7 @@ class MyDocument extends Document {
   
     return (
       <Html lang="kr">
-        <Head>
-          <meta charSet="UTF-8" />
-          <link rel="icon" href="/favicon.ico" />
-        
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
-          <link rel="canonical" href="https://xn--vk1by6xrzecngs4l6obxj.com"></link>
-          <meta name="robots" content="index,follow"></meta>
-          <meta property="og:type" content="website"></meta>
-          <meta property="og:image" content="https://xn--vk1by6xrzecngs4l6obxj.com/public/logo-circle.png" />
-          <meta property="og:url" content="https://xn--vk1by6xrzecngs4l6obxj.com"></meta>
-          <meta name="keywords" content="대한생활체육회"/>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-          
-          <meta name="naver-site-verification" content="fcd6939d5a2a544afe2de373e246bedd86d5702d" />
-          <meta name="google-site-verification" content="h0jRizviYwbetdIjDK5dX1iXSEiLWvRJ_JqBAnc7nfg" />
-          </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
