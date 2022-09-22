e8b8ead5b4ca961bc244f79c319f8781fb8f4723
eotodcp <eotodcp@gmail.com>
2022-09-22 18:09:08 +0900

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserContext.Provider value={userData}>
       <Head>
-        <title>대한생활체육회</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
             <Script
